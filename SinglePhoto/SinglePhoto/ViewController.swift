8f3144172f582fa7a0229d6680af125658a2f1a0
Jackie Wong <window6132003@yahoo.com.hk>
2016-10-30 15:30:38 +0800

    Update git repo to add apple swift project

---
+++ SinglePhoto/SinglePhoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by Scity on 13/10/2016.
+//  Copyright © 2016 Scity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
